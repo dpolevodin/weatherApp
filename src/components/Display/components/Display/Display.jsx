7350017721d47fdb55a5af3a2b20f430ce69e98b
dpolevodin <dpolevodin@gmail.com>
2022-01-11 17:29:16 +0500

    added city title

--- src/components/Display/components/Display/Display.jsx
+++ src/components/Display/components/Display/Display.jsx
@@ -19,11 +19,12 @@ export const Display = ({ weather, name, wind, sys, main }) => {
     <div className={style._}>
       {(weather && (
         <div>
+          <div className={style.cityName}>{name}</div>
           <div className={style.imageWrapper}>
             <img className={style.image} src={iconUrl} alt={weather[0].icon} />
             <div className={style.listElement}>{weather[0].description}</div>
           </div>
-          <div className={style.listElement}>Город: {name}</div>
+
           <div className={style.listElement}>
             Скорость ветра: {Math.round(wind.speed)} м/c
           </div>
